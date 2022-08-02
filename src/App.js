259f16cb620c55584640a6fecb917e7469d81775
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-08-02 10:46:08 +0500

    error tug'rilandi

--- src/App.js
+++ src/App.js
@@ -2,48 +2,62 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [cals, setClas] = useState("");
+  const [calc, setCalc] = useState("");
   const [result, setResult] = useState("");
 
   const ops = ["/", "*", "+", "-", "."];
 
   const updateCalc = (value) => {
-    // setCalc(calc + value);
-  };
-
-  const createDigits = () => {
-    const digits = [];
-
-    for (let i = 1; i < 10; i++) {
-      digits.push(<button key={i}> {i} </button>);
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
     }
+      
+    setCalc(calc + value);
+    
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    };
 
-    return digits;
-  };
+    const createDigits = () => {
+      const digits = [];
 
-  return (
-    <div className="App">
-      <div className="calculator">
-        <div className="display">
-          {/* {result ? <span>(0)</span> : ""} {calc || "0"} */}
-        </div>
-        <div className="operators">
-          <button onClick={() => updateCalc("/")}>/</button>
-          <button onClick={() => updateCalc("*")}>*</button>
-          <button onClick={() => updateCalc("=")}>+</button>
-          <button onClick={() => updateCalc("/")}>-</button>
+      for (let i = 1; i < 10; i++) {
+        digits.push(
+          <button onClick={() => updateCalc(i.toString())} key={i}>
+            {i}
+          </button>
+        );
+      }
 
-          <button>DEL</button>
-        </div>
-        <div className="digits">
-          {createDigits()}
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+      return digits;
+    };
+
+    return (
+      <div className="App">
+        <div className="calculator">
+          <div className="display">
+            {result ? <span>({result})</span> : ""} {calc || "0"}
+          </div>
+          <div className="operators">
+            <button onClick={() => updateCalc("/")}>/</button>
+            <button onClick={() => updateCalc("*")}>*</button>
+            <button onClick={() => updateCalc("=")}>+</button>
+            <button onClick={() => updateCalc("/")}>-</button>
+
+            <button>DEL</button>
+          </div>
+          <div className="digits">
+            {createDigits()}
+            <button onClick={() => updateCalc("0")}>0</button>
+            <button onClick={() => updateCalc(".")}>.</button>
+            <button>=</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
